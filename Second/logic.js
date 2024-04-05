40955e6afd580625c44854393528c012836b7c6c
thesocalledhonouredone <148259078+thesocalledhonouredone@users.noreply.github.com>
2024-04-05 09:42:22 +0530

    Simple buttons using HTML and CSS
    ...a tab bit of JS was also use

---
+++ Second/logic.js
@@ -0,0 +1,18 @@
+let youtube = 1
+let twitter = 1
+let tweet = 1
+
+function printYoutube() {
+    console.log("youtube button was clicked: " + youtube + " times")
+    youtube += 1
+}
+
+function printTwitter() {
+    console.log("twitter button was clicked: " + twitter + " times")
+    twitter += 1
+}
+
+function printTweet() {
+    console.log("tweet button was clicked: " + tweet + " times")
+    tweet += 1
+}
